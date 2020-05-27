f668fb492a8a9459aa20b67cb70021f0949513bd
sunli <sunli20140406@foxmail.com>
2020-05-27 12:08:40 +0800

    init

---
+++ src/sl/demo/leetcode/towsum/Main.java
@@ -0,0 +1,50 @@
+package sl.demo.leetcode.towsum;
+
+import java.util.Arrays;
+
+/**
+ *
+ * 1.两数之和
+ *
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+ *
+ * 示例:
+ *     给定 nums = [2, 7, 11, 15], target = 9
+ *     因为 nums[0] + nums[1] = 2 + 7 = 9
+ *     所以返回 [0, 1]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/two-sum
+ *
+ * @author sunli
+ * @date 2019/07/12
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        int[] result = twoSum(new int[]{2, 7, 11, 15}, 9);
+        System.out.println(Arrays.toString(result));
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+
+        int[] result = new int[2];
+        int length = nums.length;
+
+        for (int i = 0; i < length; i++) {
+            for (int j = i + 1; j < length; j++) {
+
+                if (nums[i] + nums[j] == target) {
+
+                    result[0] = i;
+                    result[1] = j;
+
+                    return result;
+                }
+            }
+        }
+
+        return result;
+    }
+}
